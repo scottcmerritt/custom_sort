8ad46fc6ecb9f8d240e29858a52b6d321176f569
Scott Merritt <scott.merritt@gmail.com>
2020-08-15 03:39:46 -0700

    Second commit

--- lib/custom_sort.rb
+++ lib/custom_sort.rb
@@ -3,4 +3,13 @@ require "custom_sort/version"
 module CustomSort
   class Error < StandardError; end
   # Your code goes here...
+
+  class TestSort
+    def initialize
+
+    end
+    def say_hello
+      "Hello sorters"
+    end
+  end
 end
